162c8e5c1a0fb6ea84358efd2eea3979a84e75a0
saurav singh <sauravsingh124@gmail.com>
2020-05-12 11:18:41 +0530

    indent code

--- src/components/TryCode/ProblemData.js
+++ src/components/TryCode/ProblemData.js
@@ -3,74 +3,54 @@ import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import "./ProblemData.scss";
 
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import MenuIcon from '@material-ui/icons/Menu';
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import MenuIcon from "@material-ui/icons/Menu";
 
 export default function ProblemData({ questionData }) {
   return (
     <div>
       <h4>Question Details</h4>
       <div className="navButton borderStyle">
-        <Button  >
-          Easy
-        </Button>
-        <Button
-        className="topButtons"
-          size="small"
-          color="primary"
-        >
+        <Button>Easy</Button>
+        <Button className="topButtons" size="small" color="primary">
           <NavigateBeforeIcon />
         </Button>
-        <Button
-        className="topButtons"
-          size="small"
-          color="primary"
-        >
+        <Button className="topButtons" size="small" color="primary">
           <NavigateNextIcon />
         </Button>
 
-        <Button
-        className="topButtons"
-          size="small"
-          color="primary"
-        >
+        <Button className="topButtons" size="small" color="primary">
           Problems
-        <MenuIcon fontSize="small" />
+          <MenuIcon fontSize="small" />
         </Button>
       </div>
       <div className="Question">
         {questionData.id}. {questionData.title}
       </div>
       <div dangerouslySetInnerHTML={{ __html: questionData.description }} />
-      <div className="exampleStyle" dangerouslySetInnerHTML={{ __html: questionData.example }} />
-      <div className="exampleStyle" dangerouslySetInnerHTML={{ __html: questionData.note }} />
+      <div
+        className="exampleStyle"
+        dangerouslySetInnerHTML={{ __html: questionData.example }}
+      />
+      <div
+        className="exampleStyle"
+        dangerouslySetInnerHTML={{ __html: questionData.note }}
+      />
       <div className="navButton">
-        <Button
-          size="small"
-          color="primary"
-          variant="contained"
-        >
+        <Button size="small" color="primary" variant="contained">
           <NavigateBeforeIcon fontSize="small" />
-        Prev
-               </Button>
-        <Button
-          size="small"
-          color="primary"
-          variant="contained"
-        >
+          Prev
+        </Button>
+        <Button size="small" color="primary" variant="contained">
           Next
-        <NavigateNextIcon fontSize="small" />
+          <NavigateNextIcon fontSize="small" />
         </Button>
       </div>
       <div className="similarButton">
-        <Button
-          size="small"
-          color="primary"
-          variant="contained"
-        >
+        <Button size="small" color="primary" variant="contained">
           Similar problems
-               </Button>
+        </Button>
       </div>
     </div>
   );
